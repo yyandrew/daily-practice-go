797a40e3dd8d7262b56397796ffe7c579886d025
lq201 <qqerqqer@126.com>
2022-10-07 10:24:05 +0800

    添加User#AuthByPassword方法

--- user/user.go
+++ user/user.go
@@ -3,18 +3,20 @@ package user
 import (
 	"context"
 	"dailypractice/utils"
+	"log"
 	"time"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type User struct {
 	Id               primitive.ObjectID `bson:"_id" json:"id"`
 	Email            string             `json:"email"`
-	EcryptedPassword string             `bson:"cryptedPassword" json:"ecryptedPassword"`
+	EcryptedPassword string             `bson:"cryptedPassword"`
 }
 
 type Userslice struct {
@@ -49,4 +51,13 @@ func All() Userslice {
 	}
 
 	return users
+}
+
+func (u *User) AuthByPassword(plainPW string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.EcryptedPassword), []byte(plainPW))
+	if err != nil {
+		log.Println(err)
+		return false
+	}
+	return true
 }
