97ca0550c8e03a2c17d6ee6379aa5ae6ae44f7f3
lq201 <qqerqqer@126.com>
2022-10-07 13:50:16 +0800

    添加上传功能

--- main.go
+++ main.go
@@ -6,20 +6,42 @@ import (
 	"dailypractice/utils"
 	"fmt"
 	"net/http"
+	"path/filepath"
+	"strconv"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
 func getTips(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"data":    tip.All(),
+		"data": tip.All(),
 	})
 }
 
 func getUsers(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
-		"data":    user.All(),
+		"data": user.All(),
 	})
+}
+
+func upload(c *gin.Context) {
+	fileParam := "tip-image"
+	file, _ := c.FormFile(fileParam)
+	dst := "./public/img/" + fileParam + strconv.FormatInt(time.Now().Unix(), 10) + filepath.Ext(file.Filename)
+	err := c.SaveUploadedFile(file, dst)
+
+	if err == nil {
+		c.JSON(http.StatusOK, gin.H{
+			"message":  "ok",
+			"filepath": filepath.Base(dst),
+		})
+	} else {
+		fmt.Println(err)
+		c.JSON(http.StatusNotAcceptable, gin.H{
+			"message": "upload error",
+		})
+	}
 }
 
 func login(c *gin.Context) {
@@ -27,12 +49,13 @@ func login(c *gin.Context) {
 	plainPW := c.PostForm("password")
 
 	user, err := user.FindByEmail(email)
-  fmt.Printf("emai: %s, password: %s, user: %+v", email, plainPW, user)
+	fmt.Printf("emai: %s, password: %s, user: %+v", email, plainPW, user)
 	utils.CheckError(err)
 
 	if user.AuthByPassword(plainPW) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "ok",
+			"user":    map[string]string{"email": user.Email},
 		})
 	} else {
 		c.JSON(http.StatusUnauthorized, gin.H{
@@ -43,8 +66,10 @@ func login(c *gin.Context) {
 
 func main() {
 	router := gin.Default()
+	router.Static("public", "./public")
 	router.GET("api/tips", getTips)
 	router.GET("api/users", getUsers)
 	router.POST("api/login", login)
-  router.Run(":9000")
+	router.POST("api/upload", upload)
+	router.Run(":9000")
 }
